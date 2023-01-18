1198bc634712836666ba5c59feff055225e5c385
Oluwatobi Kehinde <oluwatobi.kehinde@9mobile.com.ng>
2023-01-18 16:13:17 +0100

    Navbar Responsiveness

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const responsiveMenuButton = document.getElementById("responsive-icon-btn");
+const navMenu = document.getElementById('menu-list');
 
 responsiveMenuButton.addEventListener("click", () => {
   responsiveMenuButton.classList.toggle("open");
+  navMenu.classList.toggle('active')
 });
+
+
